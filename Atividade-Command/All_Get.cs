7b5aff75830b25ef8419aa4fbcad0e411e4c98d8
AndreFerreira20 <andre.ferreira20@unifesp.br>
2026-06-23 20:00:26 -0300

    Criacao dos comandos All e Get

---
+++ Atividade-Command/All_Get.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Command
+{
+    internal class All : Command
+    {
+        private DataBase db;
+
+        public void AllCommand(DataBase db) { this.db = db; }
+
+        public object Execute(object obj)
+        {
+            db.ShowAll();
+            return null;
+        }
+    }
+
+    internal class Get : Command
+    {
+        private DataBase db;
+
+        public void GetCommand(DataBase db) { this.db = db; }
+
+        public object Execute(object obj)
+        {
+            int id = (int)obj;
+            return db.Buscar(id);
+        }
+    }
+}
