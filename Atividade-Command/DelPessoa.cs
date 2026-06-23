9ad34db0a0cea463e56ec7373ad9e4f3bde11744
AndreSouza21 <andre.souza21@unifesp.br>
2026-06-23 16:57:36 -0300

    delete

---
+++ Atividade-Command/DelPessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Command
+{
+    internal class DelPessoa : Command
+    {
+        DataBase db;
+        public DelPessoa(DataBase db)
+        {
+            this.db = db;
+        }
+
+        public object Execute(object arg)
+        {
+            if (arg is int id)
+            {
+                db.Remover(id);
+            }
+            return null;
+        }
+
+
+
+    }
+}
