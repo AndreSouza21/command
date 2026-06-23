3fa91fa3770a51cf63148f60996f20865c2af950
AndreSouza21 <andre.souza21@unifesp.br>
2026-06-23 16:23:32 -0300

    banco provisorio

---
+++ Atividade-Command/DataBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Command
+{
+    internal class DataBase
+    {
+        private Dictionary<int, string> db = new Dictionary<int, string>();
+
+        public DataBase() { }
+
+        public void Inserir(int id, string nome)
+        {
+            if (!db.ContainsKey(id))
+            {
+                db.Add(id, nome);
+            }
+            else
+            {
+                Console.WriteLine("Erro: Este ID já existe.");
+            }
+        }
+        public string Buscar(int id)
+        {
+            if (db.TryGetValue(id, out string valor))
+            {
+                return valor;
+            }
+            return "Não encontrado";
+        }
+
+        public bool Remover(int id)
+        {
+            return db.Remove(id);
+        }
+
+
+
+    }
+}
